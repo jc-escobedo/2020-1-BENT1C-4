57d95dd41ab4990c6e304b750f0259b45bb3d07b
federico-marchese-zenfolio <fmarchese@zenfolio.com>
2020-04-18 20:48:54 -0300

    proyecto inicial

---
+++ Grupo4.InstitutoEducativo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Grupo4.InstitutoEducativo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
