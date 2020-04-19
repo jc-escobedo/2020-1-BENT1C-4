a8ed65f721146a6005a4be0650350a43b14a4d6c
Ignacio Forrester <nachoforrester@gmail.com>
2020-04-19 19:03:16 -0300

    modificaciones clase alumno

--- Grupo4.InstitutoEducativo/Models/Alumno.cs
+++ Grupo4.InstitutoEducativo/Models/Alumno.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Grupo4.InstitutoEducativo.Models
 {
     public class Alumno
     {
-        public Alumno()
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Legajo { get; set; }
+        public Carrera Carrera { get; set; }
+        public List<MateriaCursada> MateriasCursadas { get; set; }
+        
+        public Alumno(string Nombre, string Apellido, Carrera Carrera)
         {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Carrera = Carrera;
+            MateriasCursadas = new List<MateriaCursada>();
         }
     }
 }
