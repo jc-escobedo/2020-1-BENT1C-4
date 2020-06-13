6315285b2edc092c339ab118a483ffd464b1b9b7
nacho <nachoforrester@gmail.com>
2020-06-13 16:07:11 -0300

    fix Nombre y Cupo maximo de Materias

--- Grupo4.InstitutoEducativo/Models/Materia.cs
+++ Grupo4.InstitutoEducativo/Models/Materia.cs
@@ -6,13 +6,15 @@ namespace Grupo4.InstitutoEducativo.Models
 {
     public class Materia
     {
+        private const int CUPO_MAXIMO = 30;
+
         [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "La propiedad Nombre es requerida")]
         [MaxLength(100, ErrorMessage = "La longitud máxima de un Nombre es de 100 caracteres")]
         [MinLength(2, ErrorMessage = "La longitud mínima de un Nombre es de 2 caracteres")]
-        [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = "El campo admite sólo caracteres alfabéticos")]
+        [RegularExpression(@"[a-zA-Z áéíóú 0-9]*", ErrorMessage = "El campo admite sólo caracteres alfanuméricos")]
         public string Nombre { get; set; }
 
         [Display(Name = "Descripción")]
@@ -23,7 +25,7 @@ namespace Grupo4.InstitutoEducativo.Models
 
         [Display(Name = "Cupo máximo")]
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        [Range(0, CUPO_MAXIMO, ErrorMessage = "No puede ser más de  30")]
         public int CupoMaximo { get; set; }
 
         public List<ProfesorMateria> Profesores { get; set; }
